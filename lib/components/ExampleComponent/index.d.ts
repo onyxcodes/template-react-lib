4d809ef24158df98d9d440052bbeb07043781e04
Onyx <onyxganda@gmail.com>
2024-08-05 19:51:43 -0400

    fix: include build folder

---
+++ lib/components/ExampleComponent/index.d.ts
@@ -0,0 +1,7 @@
+import "./index.scss";
+import React from "react";
+export interface ExampleCmpProps {
+    classname?: string;
+}
+declare const ExampleComponent: React.FC<ExampleCmpProps>;
+export default ExampleComponent;
---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+import "./styles/index.scss";
+export { default as ExampleComponent } from "./components/ExampleComponent";
